2c8ce6809285f87d0ec69341e0044af3fbc73f14
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-06-12 00:51:05 +0530

    Pairs In An Array

--- src/arrays/PairsInAnArrayCode.java
+++ src/arrays/PairsInAnArrayCode.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 
 public class PairsInAnArrayCode {
     public static void pairsInArray(int n[]){
+        int tp=0;
         for (int i=0;i<n.length;i++){
             for (int j=i+1;j<n.length;j++){
                 System.out.print("("+n[i]+","+n[j]+")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("total Pairs is:"+tp);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
