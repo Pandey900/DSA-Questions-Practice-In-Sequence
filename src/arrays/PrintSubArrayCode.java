c944e598e9e4b4372622d9f29e810f1bb56092fe
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-06-12 22:44:05 +0530

    Printing SubArray With Worst Time Complexity i.e, O(n^3)

---
+++ src/arrays/PrintSubArrayCode.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class PrintSubArrayCode {
+    public static void printSubArray(int n[]){
+        int ts=0;
+        for (int i=0;i<n.length;i++){
+            for (int j=i;j<n.length;j++){
+                for (int k=i;k<j;k++){
+                    System.out.print(n[k]+" ");
+                    ts++;
+                }
+                System.out.println();
+            }
+//            System.out.println();
+        }
+        System.out.println("Total SubArrays is:"+ts);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an Array:");
+        int size=sc.nextInt();
+        int n[]=new int[size];
+        System.out.println("Enter the elements of an Array:");
+        for (int i=0;i<size;i++){
+            n[i]= sc.nextInt();
+        }
+        printSubArray(n);
+    }
+}
