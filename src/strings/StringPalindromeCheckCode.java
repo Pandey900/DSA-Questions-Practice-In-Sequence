69eccfbecc3201cf45b1f99522e81d96f60d80d0
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-06-21 01:46:51 +0530

    Check A String Is A Palindrome Or Not

---
+++ src/strings/StringPalindromeCheckCode.java
@@ -0,0 +1,44 @@
+package strings;
+
+import java.util.Scanner;
+
+public class StringPalindromeCheckCode {
+
+//    This is for a single word
+//    public static boolean isPalindrome(String str){
+//        for (int i=0;i<str.length()/2;i++){
+//            if (str.charAt(i)==str.charAt(str.length()-i-1)){
+//                System.out.println("Given String is a palindrome");
+//                return true;
+//            }
+//        }
+//        System.out.println("String is not a palindrome");
+//        return false;
+//    }
+
+
+//    This is for a whole line of string
+    public static boolean isPalindrome(String str) {
+        str=str.toLowerCase();
+        StringBuilder b=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            if(Character.isDigit(str.charAt(i)) || Character.isLetter(str.charAt(i))){
+                b.append(str.charAt(i));
+            }
+        }
+        for(int i=0;i<b.length()/2;i++){
+            if(b.charAt(i)==b.charAt(b.length()-1-i)){
+                System.out.println("Given String is a palindrome");
+                return true;
+            }
+        }
+        System.out.println("String is not a palindrome");
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str=sc.nextLine();
+        System.out.println(isPalindrome(str));
+    }
+}
