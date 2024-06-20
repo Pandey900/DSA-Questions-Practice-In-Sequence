4ee957a3a9c397fe9f0bae79951ee659d6bc9067
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-06-21 01:52:37 +0530

    Changes made for the case when in the first iteration only the character were not equal

--- src/strings/StringPalindromeCheckCode.java
+++ src/strings/StringPalindromeCheckCode.java
@@ -7,13 +7,13 @@ public class StringPalindromeCheckCode {
 //    This is for a single word
 //    public static boolean isPalindrome(String str){
 //        for (int i=0;i<str.length()/2;i++){
-//            if (str.charAt(i)==str.charAt(str.length()-i-1)){
-//                System.out.println("Given String is a palindrome");
-//                return true;
+//            if (str.charAt(i)!=str.charAt(str.length()-i-1)){
+//                System.out.println("Given String is NOT a palindrome");
+//                return false;
 //            }
 //        }
-//        System.out.println("String is not a palindrome");
-//        return false;
+//        System.out.println("String is a palindrome");
+//        return true;
 //    }
 
 
@@ -27,13 +27,13 @@ public class StringPalindromeCheckCode {
             }
         }
         for(int i=0;i<b.length()/2;i++){
-            if(b.charAt(i)==b.charAt(b.length()-1-i)){
-                System.out.println("Given String is a palindrome");
-                return true;
+            if(b.charAt(i)!=b.charAt(b.length()-1-i)){
+                System.out.println("Given String is NOT a palindrome");
+                return false;
             }
         }
-        System.out.println("String is not a palindrome");
-        return false;
+        System.out.println("String is a palindrome");
+        return true;
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
