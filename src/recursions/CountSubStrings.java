614ba68a66e13c1b0f8f6b1b1186eef9bf53b904
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-06-27 04:39:13 +0530

    Count Number Of Sub Strings From A String

---
+++ src/recursions/CountSubStrings.java
@@ -0,0 +1,26 @@
+package recursions;
+
+import java.util.Scanner;
+
+public class CountSubStrings {
+    public static int countSubStr(String str,int i,int j,int n){
+        if (n==1){
+            return 1;
+        }
+        if (n<=0){
+            return 0;
+        }
+        int res=countSubStr(str,i+1,j,n-1)+countSubStr(str,i,j-1,n-1)-countSubStr(str,i+1,j-1,n-2);
+        if (str.charAt(i)==str.charAt(j)){
+            res++;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str= sc.next();
+        int n=str.length();
+        System.out.println(countSubStr(str,0,n-1,n));
+    }
+}
