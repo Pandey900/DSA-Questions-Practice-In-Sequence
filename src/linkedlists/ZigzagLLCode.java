f0d262fdaf43d89abbb51c7eb1bf4b152d966002
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-07-20 20:17:48 +0530

    Zig Zag Of The LinkedList

---
+++ src/linkedlists/ZigzagLLCode.java
@@ -0,0 +1,104 @@
+package linkedlists;
+
+public class ZigzagLLCode {
+    static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+    public int size;
+
+    // Add Element At First Index
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Add Element At Last Index
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void zigZag() {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        // Find middle node
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // Reverse second half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Alternate merging
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
+    public static void main(String[] args) {
+        ZigzagLLCode ll = new ZigzagLLCode();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        System.out.println("Original list:");
+        ll.print();
+        ll.zigZag();
+        System.out.println("Zigzag list:");
+        ll.print();
+    }
+}
