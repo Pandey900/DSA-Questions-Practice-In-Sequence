01c5c2fbb7d130ee5501554aeda5e4730d5db1ee
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-07-22 20:23:24 +0530

    Inbuilt Stack Implementation Using Collections FrameWork

--- src/stack/StackB.java
+++ src/stack/StackB.java
@@ -2,53 +2,60 @@ package stack;
 
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.*;
 
 public class StackB {
 
-    static class Node{
-        int data;
-        Node next;
-        Node(int data){
-            this.data=data;
-            this.next=null;
-        }
-    }
+//   Stack Implementation Through Use Of LinkedList
 
-    static class Stack{
-        static  Node head=null;
+//    static class Node{
+//        int data;
+//        Node next;
+//        Node(int data){
+//            this.data=data;
+//            this.next=null;
+//        }
+//    }
+//
+//    static class Stack{
+//        static  Node head=null;
+//
+//        public static boolean isEmpty(){
+//            return head==null;
+//        }
+//        public static void push(int data){
+//            Node newNode=new Node(data);
+//            if (isEmpty()){
+//                head=newNode;
+//                return;
+//            }
+//            newNode.next=head;
+//            head=newNode;
+//        }
+//
+//        public static int pop(){
+//            if (isEmpty()){
+//                return -1;
+//            }
+//            int top=head.data;
+//            head=head.next;
+//            return top;
+//        }
+//
+//        public static int peek(){
+//            if (isEmpty()){
+//                return -1;
+//            }
+//            return head.data;
+//        }
+//
+//    }
 
-        public static boolean isEmpty(){
-            return head==null;
-        }
-        public static void push(int data){
-            Node newNode=new Node(data);
-            if (isEmpty()){
-                head=newNode;
-                return;
-            }
-            newNode.next=head;
-            head=newNode;
-        }
 
-        public static int pop(){
-            if (isEmpty()){
-                return -1;
-            }
-            int top=head.data;
-            head=head.next;
-            return top;
-        }
 
-        public static int peek(){
-            if (isEmpty()){
-                return -1;
-            }
-            return head.data;
-        }
 
-    }
 
-//    Stack Implementation Through Use Of LinkedList
+
 
 //    Stack With the use ArrayList
 //    static class Stack{
@@ -77,7 +84,8 @@ public class StackB {
 //        }
 //    }
     public static void main(String[] args) {
-        Stack s=new Stack();
+//        Stack s=new Stack();
+        Stack<Integer> s=new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
