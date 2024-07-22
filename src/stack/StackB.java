7f63f509f941956fd6581bc518912db06af4f622
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-07-22 19:50:52 +0530

    Stack Using ArrayList And LinkedList

--- src/stack/StackB.java
+++ src/stack/StackB.java
@@ -4,21 +4,38 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class StackB {
+
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
     static class Stack{
-        static ArrayList<Integer> list=new ArrayList<>();
+        static  Node head=null;
+
         public static boolean isEmpty(){
-            return list.size()==0;
+            return head==null;
         }
         public static void push(int data){
-            list.add(data);
+            Node newNode=new Node(data);
+            if (isEmpty()){
+                head=newNode;
+                return;
+            }
+            newNode.next=head;
+            head=newNode;
         }
 
         public static int pop(){
             if (isEmpty()){
                 return -1;
             }
-            int top=list.get(list.size()-1);
-            list.remove(list.size()-1);
+            int top=head.data;
+            head=head.next;
             return top;
         }
 
@@ -26,9 +43,39 @@ public class StackB {
             if (isEmpty()){
                 return -1;
             }
-            return list.get(list.size()-1);
+            return head.data;
         }
+
     }
+
+//    Stack Implementation Through Use Of LinkedList
+
+//    Stack With the use ArrayList
+//    static class Stack{
+//        static ArrayList<Integer> list=new ArrayList<>();
+//        public static boolean isEmpty(){
+//            return list.size()==0;
+//        }
+//        public static void push(int data){
+//            list.add(data);
+//        }
+//
+//        public static int pop(){
+//            if (isEmpty()){
+//                return -1;
+//            }
+//            int top=list.get(list.size()-1);
+//            list.remove(list.size()-1);
+//            return top;
+//        }
+//
+//        public static int peek(){
+//            if (isEmpty()){
+//                return -1;
+//            }
+//            return list.get(list.size()-1);
+//        }
+//    }
     public static void main(String[] args) {
         Stack s=new Stack();
         s.push(1);
