3f6d1a7f9ab835e88bdfac7e1d17001dbf7ad9be
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-08-07 22:56:43 +0530

    Implementation Of Stack And Queue Using Deque Code

--- src/queues/StackImpDequeCode.java
+++ src/queues/StackImpDequeCode.java
@@ -15,16 +15,39 @@ public class StackImpDequeCode {
             return dq.getLast();
         }
     }
+    static class Queue{
+        Deque<Integer> dq=new LinkedList<>();
+        public void add(int data){
+            dq.addLast(data);
+        }
+        public int remove(){
+            return dq.removeFirst();
+        }
+        public int peek(){
+            return dq.getFirst();
+        }
+
+    }
     public static void main(String[] args) {
-        Stack s=new Stack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
-        System.out.println("Top Element in the stack:"+s.peek());
-        System.out.println(s.pop());
-        System.out.println(s.pop());
-        System.out.println(s.pop());
-        System.out.println(s.pop());
+//        Stack s=new Stack();
+//        s.push(1);
+//        s.push(2);
+//        s.push(3);
+//        s.push(4);
+//        System.out.println("Top Element in the stack:"+s.peek());
+//        System.out.println(s.pop());
+//        System.out.println(s.pop());
+//        System.out.println(s.pop());
+//        System.out.println(s.pop());
+        Queue q=new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println("Top Element in the stack:"+q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
     }
 }
