63bb458e04c5ae0570ed99926e26cb2a5167623b
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-08-27 11:13:34 +0530

    PreOrder BinaryTree Build

---
+++ src/binarytree/BinaryTreesB.java
@@ -0,0 +1,44 @@
+package binarytree;
+
+import java.util.Scanner;
+
+public class BinaryTreesB {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+
+//        PreOrder Tree
+        static int index=-1;
+        public static Node buildTree(int node[]){
+            index++;
+            if (node[index]==-1){
+                return null;
+            }
+            Node newNode=new Node(node[index]);
+            newNode.left=buildTree(node);
+            newNode.right=buildTree(node);
+            return newNode;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the node:");
+        int size= sc.nextInt();
+        int node[]=new int[size];
+        System.out.println("Enter the values of the node:");
+        for (int i=0;i<size;i++){
+            node[i]= sc.nextInt();
+        }
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.buildTree(node);
+        System.out.println(root.data);
+    }
+}
