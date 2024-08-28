7300fd569ede23d86f56d4ceaa7d7557e9377483
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-08-28 23:44:15 +0530

    Diameter Of The Tree In O(N) Time Complexity Optimized Approach

--- src/binarytree/BinaryTreeC.java
+++ src/binarytree/BinaryTreeC.java
@@ -22,14 +22,32 @@ public class BinaryTreeC {
     }
 
 //    Diameter Of Tree
-    public static int diameter(Node root){
+    public static int diameter2(Node root){
         if (root==null) return 0;
-        int left_diameter=diameter(root.left);
-        int right_diameter=diameter(root.right);
+        int left_diameter=diameter2(root.left);
+        int right_diameter=diameter2(root.right);
         int left_height=height(root.left);
         int right_height=height(root.right);
         int self=left_height+right_height+1;
         return Math.max(self,Math.max(left_diameter,right_diameter));
+    }
+    static class Info{
+        int diam;
+        int ht;
+        public Info(int diam,int ht){
+            this.diam=diam;
+            this.ht=ht;
+        }
+    }
+    public static Info diameter(Node root){
+        if (root==null){
+            return new Info(0,0);
+        }
+        Info left_diam=diameter(root.left);
+        Info right_diam=diameter(root.right);
+        int final_Diam=Math.max(Math.max(left_diam.diam, right_diam.diam), left_diam.ht+ right_diam.ht+1);
+        int final_Ht=Math.max(left_diam.ht, right_diam.ht)+1;
+        return new Info(final_Diam,final_Ht);
     }
     public static void main(String[] args) {
         Node root=new Node(1);
@@ -40,6 +58,6 @@ public class BinaryTreeC {
         root.right.left=new Node(6);
         root.right.right=new Node(7);
         System.out.println(height(root));
-        System.out.println(diameter(root));
+        System.out.println(diameter(root).diam);
     }
 }
