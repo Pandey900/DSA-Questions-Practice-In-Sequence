8c9b42cc03b4c1ae5a150204defcb752999b8d79
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-08-28 14:09:03 +0530

    Diameter Of The Tree In O(N^2) Time Complexity

---
+++ src/binarytree/BinaryTreeC.java
@@ -0,0 +1,45 @@
+package binarytree;
+
+public class BinaryTreeC {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    public  static int height(Node root){
+        if (root==null){
+            return 0;
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+        return Math.max(lh,rh)+1;
+    }
+
+//    Diameter Of Tree
+    public static int diameter(Node root){
+        if (root==null) return 0;
+        int left_diameter=diameter(root.left);
+        int right_diameter=diameter(root.right);
+        int left_height=height(root.left);
+        int right_height=height(root.right);
+        int self=left_height+right_height+1;
+        return Math.max(self,Math.max(left_diameter,right_diameter));
+    }
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+        System.out.println(height(root));
+        System.out.println(diameter(root));
+    }
+}
