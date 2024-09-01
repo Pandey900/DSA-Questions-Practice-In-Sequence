4a60bef91c5055e57cd270c9613e4aaaf364697c
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-09-01 12:21:09 +0530

    Min Distance Between Nodes Using LCA(Lowest Common Ancestor)

--- src/binarytree/BinaryTreeC.java
+++ src/binarytree/BinaryTreeC.java
@@ -194,6 +194,31 @@ public class BinaryTreeC {
         }
         return root;
     }
+
+
+    public static int lcaDist(Node root,int n){
+        if (root==null){
+            return -1;
+        }
+        if (root.data==n){
+            return 0;
+        }
+        int leftDist=lcaDist(root.left,n);
+        int rightDist=lcaDist(root.right,n);
+        if (leftDist==-1&&rightDist==-1){
+            return -1;
+        } else if (leftDist==-1) {
+            return rightDist+1;
+        }else {
+            return leftDist+1;
+        }
+    }
+    public static int minDist(Node root,int n1,int n2){
+        Node lca=lca2(root,n1,n2);
+        int dist1=lcaDist(root,n1);
+        int dist2=lcaDist(root,n2);
+        return dist1+dist2;
+    }
     public static void main(String[] args) {
         Node root=new Node(1);
         root.left=new Node(2);
@@ -212,8 +237,9 @@ public class BinaryTreeC {
 //        topView(root);
 //        int k=2;
 //        printKth(root,1,k);
-        int n1=4,n2=5;
+        int n1=4,n2=6;
 //        System.out.println(getLca(root,n1,n2).data);
-        System.out.println(lca2(root,n1,n2).data);
+//        System.out.println(lca2(root,n1,n2).data);
+        System.out.println(minDist(root,n1,n2));
     }
 }
