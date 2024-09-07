fc93968730462f3ecc86c5976b264b32ee62b4b5
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-09-07 13:10:39 +0530

    Calculating Max BST IN The BT

--- src/binarysearchtreeproblems/BinarySearchTreeA.java
+++ src/binarysearchtreeproblems/BinarySearchTreeA.java
@@ -226,6 +226,40 @@ public class BinarySearchTreeA {
         root=createBST(inorder,0,inorder.size()-1);
         return root;
     }
+
+
+//Calculating Max BST in BT
+    static class Info{
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+        Info(boolean isBST,int  size,int min,int max){
+            this.isBST=isBST;
+            this.size=size;
+            this.min=min;
+            this.max=max;
+        }
+    }
+    public static int maxBST=0;
+    public static Info largestBST(Node root){
+        if (root==null){
+            return new Info(true,0,Integer.MAX_VALUE,Integer.MIN_VALUE);
+        }
+        Info leftInfo=largestBST(root.left);
+        Info rightInfo=largestBST(root.right);
+        int size= leftInfo.size+rightInfo.size+1;
+        int min=Math.min(root.data,Math.min(leftInfo.min,rightInfo.min));
+        int max=Math.max(root.data,Math.max(leftInfo.max, rightInfo.max));
+        if (root.data<=leftInfo.max||root.data>=rightInfo.min){
+            return new Info(false,size,min,max);
+        }
+        if (leftInfo.isBST&& rightInfo.isBST){
+            maxBST=Math.max(maxBST,size);
+            return new Info(true,size,min,max);
+        }
+        return new Info(false,size,min,max);
+    }
     public static void main(String[] args) {
 //        int values[]={8,5,3,1,4,6,10,11,14};
 //        int values[]={1,1,1,1};
@@ -271,15 +305,28 @@ public class BinarySearchTreeA {
 //        Node root=createMinBST(arr,0,arr.length-1);
 //        preorder(root);
 
-        Node root=new Node(8);
-        root.left=new Node(6);
-        root.left.left=new Node(5);
-        root.left.left.left=new Node(3);
+//        Node root=new Node(8);
+//        root.left=new Node(6);
+//        root.left.left=new Node(5);
+//        root.left.left.left=new Node(3);
+//
+//        root.right=new Node(10);
+//        root.right.right=new Node(11);
+//        root.right.right.right=new Node(12);
+//        root=balancedBST(root);
+//        preorder(root);
 
-        root.right=new Node(10);
-        root.right.right=new Node(11);
-        root.right.right.right=new Node(12);
-        root=balancedBST(root);
-        preorder(root);
+
+        Node root=new Node(50);
+        root.left=new Node(30);
+        root.left.left=new Node(5);
+        root.left.right=new Node(20);
+        root.right=new Node(60);
+        root.right.right=new Node(70);
+        root.right.right.right=new Node(80);
+        root.right.right.left=new Node(65);
+        root.right.left=new Node(45);
+        Info info=largestBST(root);
+        System.out.println(maxBST);
     }
 }
