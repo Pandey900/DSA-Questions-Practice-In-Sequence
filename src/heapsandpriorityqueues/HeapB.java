4a58cd995202145a48d56fe62e2f8c015aaef713
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-09-10 23:06:54 +0530

    Add And Peek Data From Heap

---
+++ src/heapsandpriorityqueues/HeapB.java
@@ -0,0 +1,26 @@
+package heapsandpriorityqueues;
+
+import java.util.ArrayList;
+
+public class HeapB {
+    static class Heap{
+        ArrayList<Integer> arr=new ArrayList<>();
+        public void add(int data){
+            arr.add(data);
+            int x=arr.size()-1;
+            int par=(x-1)/2;
+            while (arr.get(x)<arr.get(par)){
+                int temp=arr.get(x);
+                arr.set(x,arr.get(par));
+                arr.set(par,temp);
+            }
+        }
+        public int peek(){
+            return arr.get(0);
+        }
+    }
+    public static void main(String[] args) {
+        HeapB pq=new HeapB();
+
+    }
+}
