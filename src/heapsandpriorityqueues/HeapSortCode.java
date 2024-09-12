d9d291d6b5adeeb276790ef95b5f072c5a1fd7b8
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-09-12 14:32:24 +0530

    Heap Sort Using MinHeap Descending Order

--- src/heapsandpriorityqueues/HeapSortCode.java
+++ src/heapsandpriorityqueues/HeapSortCode.java
@@ -5,19 +5,41 @@ public class HeapSortCode {
     public static void heapify(int arr[],int i,int size){
         int left=2*i+1;
         int right=2*i+2;
-        int maxIndex=i;
-        if (left<size&&arr[left]>arr[maxIndex]){
-            maxIndex=left;
-        }
-        if (right<size&&arr[right]>arr[maxIndex]){
-            maxIndex=right;
-        }
-        if (maxIndex!=i){
-            int temp=arr[i];
-            arr[i]=arr[maxIndex];
-            arr[maxIndex]=temp;
+//        int maxIndex=i;
+        int minIndex=i;
+//        Ascending Order MaxHeap
+//        if (left<size&&arr[left]>arr[maxIndex]){
+//            maxIndex=left;
+//        }
+//        if (right<size&&arr[right]>arr[maxIndex]){
+//            maxIndex=right;
+//        }
 
-            heapify(arr,maxIndex,size);
+//        Descending Order MinHeap
+        if (left<size&&arr[left]<arr[minIndex]){
+            minIndex=left;
+        }
+        if (right<size&&arr[right]<arr[minIndex]){
+            minIndex=right;
+        }
+
+
+//        Ascending Order MaxHeap
+//        if (maxIndex!=i){
+//            int temp=arr[i];
+//            arr[i]=arr[maxIndex];
+//            arr[maxIndex]=temp;
+//
+//            heapify(arr,maxIndex,size);
+//        }
+
+//        Descending Order MinHeap
+        if (minIndex!=i){
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+
+            heapify(arr,minIndex,size);
         }
     }
     public static void heapSort(int arr[]){
