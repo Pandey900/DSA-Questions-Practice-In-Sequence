73e9be7f795789b1d44c3b6e20ee7cb257c145d3
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-09-14 15:02:48 +0530

    SecondLargest Element In The Array

---
+++ src/geeksforgeekscongnizant/ArraysQuestions/SecondLargestElementProblemCode.java
@@ -0,0 +1,22 @@
+package geeksforgeekscongnizant.ArraysQuestions;
+
+public class SecondLargestElementProblemCode {
+    public static int secondLargestElement(int arr[]){
+        int first=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]>first){
+                second=first;
+                first=arr[i];
+            } else if (arr[i]>second&&first!=second) {
+                second=arr[i];
+            }
+        }
+        return (second==Integer.MIN_VALUE)?-1:second;
+    }
+    public static void main(String[] args) {
+        int arr[]={12,35,1,10,34,1};
+        int secondLargest=secondLargestElement(arr);
+        System.out.println(secondLargest);
+    }
+}
