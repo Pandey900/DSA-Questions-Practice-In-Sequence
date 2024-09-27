2e9136c60e608cb104e2639213190e296abb0a96
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-09-27 23:56:28 +0530

    Practice Hollow Rectangle, Inverted And Half Pyramid, Inverted And Half Pyramid Numbers, Floyd's Triangle

--- src/practiceproblems/AdvancedPatterns.java
+++ src/practiceproblems/AdvancedPatterns.java
@@ -32,9 +32,20 @@ public class AdvancedPatterns {
             System.out.println();
         }
     }
+    public static void floydTriangle(int n){
+        int counter=1;
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
 //        hollowRectangle(4,5);
 //        invertedAndHalfPyramid(4);
-        invertedAndHalfPyramidNumber(5);
+//        invertedAndHalfPyramidNumber(5);
+        floydTriangle(4);
     }
 }
