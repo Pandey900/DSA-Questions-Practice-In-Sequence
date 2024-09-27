f9a94fb8fbf0467e2d95ed4c0a4d959845b438d7
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-09-27 23:51:22 +0530

    Practice Hollow Rectangle, Inverted And Half Pyramid

--- src/practiceproblems/AdvancedPatterns.java
+++ src/practiceproblems/AdvancedPatterns.java
@@ -1,6 +1,6 @@
 package practiceproblems;
 
-public class HollowRectangleProblem {
+public class AdvancedPatterns {
     public static void hollowRectangle(int totRow,int totCol){
         for (int i=0;i<=totRow;i++){
             for(int j=0;j<=totCol;j++){
@@ -13,7 +13,20 @@ public class HollowRectangleProblem {
             System.out.println();
         }
     }
+    public static void invertedAndHalfPyramid(int n){
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        hollowRectangle(4,5);
+//        hollowRectangle(4,5);
+        invertedAndHalfPyramid(4);
+
     }
 }
