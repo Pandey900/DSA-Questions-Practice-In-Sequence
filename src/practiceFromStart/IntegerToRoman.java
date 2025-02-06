5535e5f07c53ddc4c1c50affc5b0c1e94556ce81
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2025-02-06 20:17:44 +0530

    Practicing Questions

--- src/practiceFromStart/IntegerToRoman.java
+++ src/practiceFromStart/IntegerToRoman.java
@@ -3,6 +3,20 @@ package practiceFromStart;
 import java.util.Scanner;
 
 public class IntegerToRoman {
+//    We Can Solve  This By Subtraction Also But That Will Not Be The Efficient Method
+    public static String integerToRoman1(int n){
+        StringBuilder roman=new StringBuilder();
+        int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String letter[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        for(int i=0;i<values.length;i++){
+            while (n>=values[i]){
+                roman.append(letter[i]);
+                n-=values[i];
+            }
+        }
+        return roman.toString();
+    }
+//    This is Division Method And It Is Most Efficient Method In Terms Of Space And Time Complexity
     public static String integerToRoman(int n){
         StringBuilder roman=new StringBuilder();
         int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
@@ -22,5 +36,6 @@ public class IntegerToRoman {
         System.out.println("Enter the Number:");
         int n= sc.nextInt();
         System.out.println(integerToRoman(n));
+        System.out.println(integerToRoman1(n));
     }
 }
