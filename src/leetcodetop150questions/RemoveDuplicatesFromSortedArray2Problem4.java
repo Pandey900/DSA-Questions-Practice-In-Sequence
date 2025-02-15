beb96e1c5d2dc416f63ebd1cae4ebded80a7ddc4
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2025-02-15 10:58:44 +0530

    LeetCode 150 Questions Solving, This is Question-4(RemoveDuplicatesFromSorted Array2)

---
+++ src/leetcodetop150questions/RemoveDuplicatesFromSortedArray2Problem4.java
@@ -0,0 +1,27 @@
+package leetcodetop150questions;
+
+import java.util.Scanner;
+
+public class RemoveDuplicatesFromSortedArray2Problem4 {
+    public static int removeDuplicatesFromSortedArray2(int arr[]){
+        int i=0;
+        for (int j=0;j<arr.length;j++){
+            if (i<2 || arr[j]!=arr[i-2]){
+                arr[i]=arr[j];
+                i++;
+            }
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array:");
+        int size= sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements of Array:");
+        for (int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(removeDuplicatesFromSortedArray2(arr));
+    }
+}
